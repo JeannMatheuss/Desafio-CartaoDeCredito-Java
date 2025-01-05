1817248924be32792ab6cf23e92e335cc44c6e63
Jean <jeanmatheus0612@gmail.com>
2025-01-04 22:23:13 -0300

    Metodo para comparar

--- src/Compra.java
+++ src/Compra.java
@@ -1,5 +1,5 @@
 
-public class Compra {
+public class Compra implements Comparable<Compra>{
     private String descricao;
     private double valor;
 
@@ -23,5 +23,11 @@ public class Compra {
     public String toString() {
         return "Compra: descrição: " + descricao + 
                 " valor: " + valor;
+    }
+
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
     }
 }
