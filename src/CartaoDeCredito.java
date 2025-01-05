185aba933ca9952498ea2b698c0867aa53c61232
Jean <jeanmatheus0612@gmail.com>
2025-01-04 21:58:31 -0300

    Criando o construtor do CartaoDeCredito

--- src/CartaoDeCredito.java
+++ src/CartaoDeCredito.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class CartaoDeCredito {
     private double limite;
     private double saldo;
     private List<Compra> compras;
+
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+
 }
